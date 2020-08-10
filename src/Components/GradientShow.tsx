f8f578820ce845729b61b8612bef161a0e543954
Andy Tran <akitran@ucdavis.edu>
2020-08-10 07:25:12 -0700

    Dev gradient show

--- src/Components/GradientShow.tsx
+++ src/Components/GradientShow.tsx
@@ -1,10 +1,32 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useRef, useState, useEffect } from 'react';
+import { Canvas } from 'react-three-fiber'
+import { useSpring, a } from 'react-spring/three'
+import * as THREE from 'three/';
 
 interface GradientShowProps {
 	props: any
+	gradientActive: Boolean
 }
 
-export const GradientShow: FunctionComponent<GradientShowProps> = ({ props }) => {
+export const GradientShow: FunctionComponent<GradientShowProps> = ({ props, gradientActive }) => {
+	// the position of the light will change and move to specified locations according to length of swipe
+	// movement in the x or y axis will move the light and @ specific length will also begin to change the color of the background 
+	const planeRef = useRef<THREE.Object3D | undefined>(undefined);
+	const [mounted, setMounted] = useState<Boolean>(false);
+
+	// define a moving, hold, and direction state -- hold will stop movement where it's at, moving will move light to same percentage of completion as swipe, 
+	// pointerUp will either finish to next state or revert to previous state if threshold is not reached
+
+	// paremeters: light positions and background colors and text	
+
+	const rectLightAnimation = useSpring({
+		position: !mounted ? [220, 0, 0] : [-150, 0, 0],
+	});
+
+	useEffect(() => {
+		!mounted && setMounted(true);
+	}, [mounted])
+
 	return (
 		<div className="GradientShow" style={props}>
 			<div className="grid">
@@ -12,6 +34,22 @@ export const GradientShow: FunctionComponent<GradientShowProps> = ({ props }) =>
 					<p>The most damaging phrase in language is 'It's always been done that way.'</p>
 					<p className="spacer-top-1">- Admiral Grace Hopper</p>
 				</div>
+				<Canvas
+					className="gradient"
+				>
+					<ambientLight intensity={0.7} />
+					<a.rectAreaLight
+						position={rectLightAnimation.position}
+						rotation={[0, 0, -Math.PI/12]}
+						intensity={3}
+						width={180}
+						height={600}
+						lookAt={() => planeRef.current && planeRef.current.position} />
+					<mesh position={[0, 0, -150]} ref={planeRef}>
+						<planeBufferGeometry attach='geometry' args={[1000, 1000]}/>
+						<meshPhysicalMaterial attach='material' color={new THREE.Color(0x9986FC)} />
+					</mesh>
+				</Canvas>
 			</div>
 		</div>
 	)
--- src/Components/MobileAnimation.tsx
+++ src/Components/MobileAnimation.tsx
@@ -5,10 +5,12 @@ import { Canvas } from 'react-three-fiber';
 import { Sphere } from './Sphere';
 import { GradientShow } from './GradientShow';
 import { MobileEmailForm } from './MobileEmailForm';
+// import { useSpring, a } from 'react-spring/three';
 
 export const MobileAnimation: FunctionComponent = () => {
 	// mobile animation will handle state of the animation components
 	// const [darkMode, setDarkMode] = useState<Boolean>(false);
+
 	const [gradientActive, setGradientActive] = useState<Boolean>(false);
 	const [sphereState, setSphereState] = useState<{ hold: Boolean, direction: null | 'forwards' | 'backwards' }>({ hold: false, direction: null});
 	const [emailVisible, setEmailVisible] = useState<Boolean>(true);
@@ -18,14 +20,9 @@ export const MobileAnimation: FunctionComponent = () => {
 			<div className="logo">
 				<img src='../assets/logo.svg' alt='Logo' />
 			</div>
-			<Transition
-				items={gradientActive}
-				from={{ backgroundColor: 'transparent', opacity: 0}}
-				enter={{ opacity: 1 }}
-			>
-				{gradientActive => gradientActive && (props => <GradientShow props={props} />)}
-			</Transition>
 			<Canvas
+				// onClick={() => setGradientActive(true)}
+				className="Canvas"
 				camera={{position: [0, 0, 500]}}
 			>
 				<Sphere
@@ -39,6 +36,13 @@ export const MobileAnimation: FunctionComponent = () => {
 					breakPoint={468}
 				/>
 			</Canvas>
+			<Transition
+				items={gradientActive}
+				from={{ backgroundColor: 'transparent', opacity: 0}}
+				enter={{ opacity: 1 }}
+			>
+				{gradientActive => gradientActive && (props => <GradientShow props={props} gradientActive={gradientActive} />)}
+			</Transition>
 			{/* <div className="hold-icon">
 				<img src="../assets/holdicon.svg" alt="Press and hold to learn more about Andy Mag"/>
 				<p>Press and hold to learn more about Andy Mag</p>
