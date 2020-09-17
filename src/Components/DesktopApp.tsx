55f553616e6cc0e8ad0f1940aaa632d3c36eff7f
Andy Tran <akitran@ucdavis.edu>
2020-09-17 01:19:36 -0700

    Added test wheel event listeners onto test elements

--- src/Components/DesktopApp.tsx
+++ src/Components/DesktopApp.tsx
@@ -3,15 +3,15 @@ import React, { FunctionComponent } from 'react';
 export const DesktopApp: FunctionComponent = () => {
 	return (
 		<>
-		<div className="div1" onWheel={(e) => console.log(e.target)}>
-			HELLO
-		</div>
-		<div
-			className="div2"
-			onWheel={(e) => console.log(e.target)}
-		>
-			WHAT'S UP
-		</div>
+			<div className="div1" onWheel={(e) => console.log(e.target)}>
+				HELLO
+			</div>
+			<div
+				className="div2"
+				onWheel={(e) => console.log(e.target)}
+			>
+				WHAT'S UP
+			</div>
 		</>
 	)
 }
