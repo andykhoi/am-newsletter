e0eb690eed4648aa51a7954902b6268b5174a3b1
Andy Tran <akitran@ucdavis.edu>
2020-09-25 14:35:57 -0700

    added build file

---
+++ build/precache-manifest.d0b55579ae862b9295605eb19cfed535.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fed18ebff3394df4f39fd903146a8f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d16f178e2a32d6949ba7",
+    "url": "/static/css/main.bbf2ace8.chunk.css"
+  },
+  {
+    "revision": "474a12459de9bdb27941",
+    "url": "/static/js/2.228edf5f.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.228edf5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d16f178e2a32d6949ba7",
+    "url": "/static/js/main.74180006.chunk.js"
+  },
+  {
+    "revision": "f43d92086837b706fbcd",
+    "url": "/static/js/runtime-main.13a864f6.js"
+  },
+  {
+    "revision": "d321fa78bb7190a8ca7e14213ef63203",
+    "url": "/static/media/Aileron-Regular.d321fa78.otf"
+  },
+  {
+    "revision": "a037b26f5815caf890a1589cfe2576f9",
+    "url": "/static/media/overpass-bold.a037b26f.otf"
+  },
+  {
+    "revision": "018fb40a392564338367765c7ffd4c5f",
+    "url": "/static/media/overpass-extrabold.018fb40a.otf"
+  }
+]);
